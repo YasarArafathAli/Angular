b646d6a475d965209da062009f32f2296b0ddc6f
Yasar Arafath <yasara218@gmail.com>
2020-08-31 12:17:32 +0530

    angular start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+// import { HttpModule } from '@angular/'
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { BaseComponent } from './base/base.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    BaseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+    // HttpModule
   ],
-  providers: [],
+  providers: [],  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
